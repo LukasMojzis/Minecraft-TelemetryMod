17038aa4f2d157d9a5b06e0f3e19d7fcc1b1ff89
Lukáš Mojžíš <lukas.mojzis@grenames.cz>
2023-07-23 20:29:38 +0200

    Add getWorldCreationTime method to WorldCalendar class

--- src/main/java/cz/lukasmojzis/telemetrymod/WorldCalendar.java
+++ src/main/java/cz/lukasmojzis/telemetrymod/WorldCalendar.java
@@ -1,9 +1,36 @@
 package cz.lukasmojzis.telemetrymod;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+
 /**
  * The WorldCalendar class provides functionality related to world time ticks from the world.
  */
 public class WorldCalendar {
+
+    /**
+     * This method retrieves the creation time of the world currently loaded in the {@link TelemetryMod}.
+     *
+     * <p>It works by accessing the directory where the world is stored,
+     * and fetching the 'creationTime' attribute of the directory itself.
+     * This attribute represents the time at which the world directory was created.
+     *
+     * @return The creation time of the world directory, represented as the number of milliseconds since the Unix Epoch.
+     * @throws Error if an exception is encountered while trying to calculate the world creation time.
+     *               This is a fatal error as the method is not expected to fail under normal circumstances.
+     */
+    static long getCreationTime() {
+        try {
+            File worldDir = TelemetryMod.currentWorld.getSaveHandler().getWorldDirectory();
+            BasicFileAttributes attr = Files.readAttributes(worldDir.toPath(), BasicFileAttributes.class);
+            return attr.creationTime().toMillis();
+        } catch (Exception e) {
+            throw new Error("Failed to calculate the world creation time");
+        }
+    }
+
+
     /**
      * Returns the total number of world time ticks from the world.
      *
